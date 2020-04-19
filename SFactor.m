4c151dc4ec5ebca3c2985c71035b669ecdc84473
Mauro Lacy <mauro@lacy.com.es>
2020-04-19 18:03:42 +0200

    SFactor module / package

---
+++ SFactor.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*BeginPackage["SFactor`", {"SFactor`"}]*)
+
+
+(* ::Input:: *)
+(*SFactor[p_,k_,func_:Apart]:=HoldForm[StandardForm[k]]*StandardForm[func@(p*1/k)]*)
+
+
+(* ::Input:: *)
+(*EndPackage[]*)
---
+++ SFactor.wl
@@ -0,0 +1,4 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*SFactor[p_,k_,func_:Apart]:=HoldForm[StandardForm[k]]*StandardForm[func@(p*1/k)]*)
